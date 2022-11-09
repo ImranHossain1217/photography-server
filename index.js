bca445ecc2b7ab37c85231425d0e2b0b254d1f89
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-10 02:55:25 +0600

    midllewares added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send('Photography website server running!!');
