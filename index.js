339b229f701986a436352d3deeb63938a1769b30
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-10 22:20:12 +0600

    delete single review api

--- index.js
+++ index.js
@@ -1,62 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vwrnpfj.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run(){
-    try{
-       const serviceCollection = client.db('Photography').collection('services');
-       const reviewCollection = client.db('Photography').collection('reviews');
-
-      // services get api from db
-       app.get('/services', async(req, res) => {
-        const query = {};
-        const cursor =  serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-       }); 
-
-       app.get('/services/:id', async(req,res) => {
-           const id = req.params.id
-           const query = {_id:ObjectId(id)};
-           const services = await serviceCollection.findOne(query);
-           res.send(services);
-       });
-
-       // add review api
-       app.post('/reviews', async(req,res) => {
-          const review = req.body;
-          const result =await reviewCollection.insertOne(review);
-          res.send(result);
-       });
-
-       app.get('/reviews', async(req,res)=> {
-         const query = {};
-         const cursor = reviewCollection.find(query);
-         const reviews = await cursor.toArray();
-         res.send(reviews);
-       });
-    }
-    finally{
-
-    }
-};
-run().catch(err => console.log(err));
-
-app.get('/',(req, res) => {
-    res.send('Photography website server running!!');
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port,()=> {
-    console.log(`Photography website running on ${port}`)
+async function run() {
+  try {
+    const serviceCollection = client.db("Photography").collection("services");
+    const reviewCollection = client.db("Photography").collection("reviews");
+
+    // services get api from db
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const services = await serviceCollection.findOne(query);
+      res.send(services);
+    });
+
+    // add review api
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Photography website server running!!");
+});
+
+app.listen(port, () => {
+  console.log(`Photography website running on ${port}`);
 });
